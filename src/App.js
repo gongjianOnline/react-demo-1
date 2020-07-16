c2768de66a0be905c6d9e799b1672d5948f3f2f9
李承斌 <15031617367@163.com>
2020-07-16 16:17:51 +0800

    在函数组件中自定义了useUpdate监听钩子

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import React,{useState,useEffect} from 'react';
-
-// class App extends React.Component{
-//   constructor(props) {
-//     super(props);
-//     this.state = {n:1}
-//   };
-//   onClick= ()=>{
-//     this.setState((state)=>({
-//       n:state.n+1
-//     }))
-//   };
-//   render(){
-//     return (
-//         <div>
-//           <span>{this.state.n}</span>
-//           <button onClick={this.onClick}>add</button>
-//         </div>
-//     )
-//   }
-// }
+import useUpdate from "./useUpdate"
 
 
 const App = (props)=>{ //消除了 this
   const [n,setN] = useState(0);
-  const [m,setM] = useState(0);
   const onClickN = ()=>{
     setN(n+1)
   }
-  const onClickM = ()=>{
-    setM(m+1)
-  }
-  useEffect(()=>{
-    console.log("第一次渲染")
-  },[]);
-  useEffect(()=>{
+  
+  //调用变化的监听
+  useUpdate(()=>{
     console.log("n update")
-  },[n])
-  useEffect(()=>{
-    console.log("n AND m update")
-  },[n,m])
+  },n)
+
+
 
   return (
       <div>
@@ -46,11 +21,6 @@ const App = (props)=>{ //消除了 this
           {n}
           <button onClick={onClickN}>add</button>
         </div>
-        <div>
-          {m}
-          <button onClick={onClickM}>add</button>
-        </div>
-
       </div>
   )
 }
