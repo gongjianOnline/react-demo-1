03c0ec496075add922610eed7bdf47c79a11df41
李承斌 <15031617367@163.com>
2020-07-17 14:23:13 +0800

    useRef使用

--- src/index.js
+++ src/index.js
@@ -1,58 +1,24 @@
-import React, {useEffect} from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+const rootElement = document.getElementById("root");
 
-//useState实现思路
-/**
- *更改函数实现缺点
- * 若第一次渲染是n第一个,m是第二个,k是第三个
- * 则第二次渲染师必须保证顺序完全一致
- * (app组件中)
- * if(n%2===1){
- *      [m,setM] = useState(0)
- * }
- * */
-let _state = [];
-let index = 0;
-const myuseState = (value)=>{
-    let blockIndex = index;
-    _state[blockIndex] = _state[blockIndex]===undefined?value:_state[blockIndex];
-    const setState = (newValue)=>{
-        _state[blockIndex] = newValue;
-        console.log(_state)
-        render()
-    }
-    index++
-    return [_state[blockIndex],setState]
-}
-const render = ()=>{
-    index=0
-    return ReactDOM.render(<App />, document.getElementById('root'));
-}
-
-
+//useRef使用
+//强制更新的例子
 function App(){
-    const [n,setN] = myuseState(0);
-    const [m,setM] = myuseState(0);
+    const nRef = React.useRef(0)
+    const update = React.useState()[1]
     const add = ()=>{
-        setN(n+1)
+        return ((nRef.current++),update(nRef.current))
     }
-    const addM = ()=>{
-        setM(m+1)
-    }
-    useEffect(()=>{
-        console.log("n update")
-    },[n])
     return (
         <div>
-            <p>{n}</p>
+            <p>{nRef.current}</p>
             <button onClick={add}>add</button>
-            <p>{m}</p>
-            <button onClick={addM}>add</button>
+            <button>log</button>
         </div>
     )
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+
+ReactDOM.render(<App />, rootElement);
