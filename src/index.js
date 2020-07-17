6f2fafd927f9a53a717ab9769420797bd023b87e
李承斌 <15031617367@163.com>
2020-07-17 11:16:29 +0800

    useState简单原理实现

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+//useState实现思路
+let _state = null;
+const myuseState = (value)=>{
+    _state = _state===null?value:_state;
+    const setState = (newValue)=>{
+        _state = newValue;
+        console.log(_state)
+        render()
+    }
+    return [_state,setState]
+}
+const render = ()=>ReactDOM.render(<App />, document.getElementById('root'));
+
+
+function App(){
+    const [n,setN] = myuseState(0);
+    const add = ()=>{
+        setN(n+1)
+    }
+    useEffect(()=>{
+        console.log("n update")
+    },[n])
+    return (
+        <div>
+            <p>{n}</p>
+            <button onClick={add}>add</button>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
 
 
 ReactDOM.render(
