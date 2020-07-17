2dcfdace7e3d4c92589717fcd0d7c6fec8798b22
李承斌 <15031617367@163.com>
2020-07-17 13:26:55 +0800

    如何让两个useState不冲突

--- src/index.js
+++ src/index.js
@@ -2,25 +2,33 @@ import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 //useState实现思路
-let _state = null;
+let _state = [];
+let index = 0;
 const myuseState = (value)=>{
-    _state = _state===null?value:_state;
+    let blockIndex = index;
+    _state[blockIndex] = _state[blockIndex]===undefined?value:_state[blockIndex];
     const setState = (newValue)=>{
-        _state = newValue;
+        _state[blockIndex] = newValue;
         console.log(_state)
         render()
     }
-    return [_state,setState]
+    index++
+    return [_state[blockIndex],setState]
 }
 const render = ()=>{
+    index=0
     return ReactDOM.render(<App />, document.getElementById('root'));
 }
 
 
 function App(){
     const [n,setN] = myuseState(0);
+    const [m,setM] = myuseState(0);
     const add = ()=>{
         setN(n+1)
+    }
+    const addM = ()=>{
+        setM(m+1)
     }
     useEffect(()=>{
         console.log("n update")
@@ -29,6 +37,8 @@ function App(){
         <div>
             <p>{n}</p>
             <button onClick={add}>add</button>
+            <p>{m}</p>
+            <button onClick={addM}>add</button>
         </div>
     )
 }
