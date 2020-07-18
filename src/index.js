ecf5952120d4a83dd5cd36d5ef0aa277be25a1a7
龚箭 <15031617367@163.com>
2020-07-18 20:00:44 +0800

    React.memo和useMemo和useCallback

--- src/index.js
+++ src/index.js
@@ -1,37 +1,61 @@
-import React, {createContext, useContext, useEffect, useReducer, useState} from "react";
+import React, {createContext, useCallback, useContext, useEffect, useMemo, useReducer, useState} from "react";
 import ReactDOM from "react-dom";
 const rootElement = document.getElementById("root");
 
-//初使用useEffect和uselayoutEffect
-/*
-* useEffect的执行时机是在先虚拟DOM转换成DOM然后直接渲染UI后再执行useEffect(推荐 优选渲染UI)
-* uselayoutEffect的执行时机是虚拟DOM转换成DOM后先执行layoutEffect然后在执行渲染UI的操作(用法和useEffect相同,只是执行时机不同)
-*
-* */
-
+//初使用useMemo和React.memo
+//用于防止App组件对子组件进行重复渲染,只要props不变就不会重新渲染子组件
 
 function App(){
+    console.log("调用了app")
     const [n,setN] = useState(0)
-    const add =()=>{
-        setN((state)=>{return state+1})
+    const [m,setM] = useState(0)
+    const addN = ()=>{
+        setN((state)=>{
+            return state+1
+        })
     }
-    useEffect(()=>{
-        let id = setInterval(()=>{ //页面初始化的时候调用定时器
-            console.log("页面初始化的定时器")
-        });
-        return ()=>{ //页面销毁是清空定时器
-            return window.clearInterval(id)
-        }
-    },[])
-    return(
+    // const addM = useMemo(()=>{
+    //     return ()=>{
+    //                 setM((state)=>{
+    //                     return state+1
+    //                 })
+    //             }
+    // },[m])
+
+    //useMemo的语法糖
+    const addM =useCallback(
+        ()=>{
+            setM((state)=>{
+                return state+1
+            })
+        },[m]
+    )
+
+
+    return (
         <div>
-            {n}
-            <button onClick={add}>add</button>
-
-
+            <div>
+                prend:{n}
+                <button onClick={addN}>addN</button>
+            </div>
+            {/*<Child2 data={m} onClick={addM}></Child2>*/}
+            <Child2 data={m} onClick={addM}></Child2>
         </div>
     )
+
 }
+
+const Child2 = React.memo((props)=>{
+    console.log("调用了CHild")
+    return(
+        <div>
+            Child: {props.data}
+            <button onClick={props.onClick}>addM</button>
+        </div>
+    )
+})
+
+
 
 
 
