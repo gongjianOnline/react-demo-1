19a9e9507596175df8734b2b3f68eed3ddc036ec
龚箭 <15031617367@163.com>
2020-07-18 13:29:12 +0800

    useState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 const rootElement = document.getElementById("root");
 
 
+function App(){
+    const [n,setN] = useState(0)
+    const add = ()=>{
+        setN((state)=>{
+            return state+1
+        })
+    }
+
+
+    return (
+        <div className='App'>
+            <h1>n:{n}</h1>
+            <button onClick={add}>add</button>
+
+        </div>
+    )
+
+}
 
 
 ReactDOM.render(<App />, rootElement);
