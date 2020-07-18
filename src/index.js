b291930cbfafee153ce0341a6e8b34c61690c2e5
龚箭 <15031617367@163.com>
2020-07-18 13:22:37 +0800

    day06INIT

--- src/index.js
+++ src/index.js
@@ -2,22 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 const rootElement = document.getElementById("root");
 
-//useRef使用
-//强制更新的例子
-function App(){
-    const nRef = React.useRef(0)
-    const update = React.useState()[1]
-    const add = ()=>{
-        return ((nRef.current++),update(nRef.current))
-    }
-    return (
-        <div>
-            <p>{nRef.current}</p>
-            <button onClick={add}>add</button>
-            <button>log</button>
-        </div>
-    )
-}
 
 
 
