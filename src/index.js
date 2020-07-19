a8b7d951de8973943e7e9a7343dd68ecee9f478c
龚箭 <15031617367@163.com>
2020-07-19 17:23:49 +0800

    useRef用法

--- src/index.js
+++ src/index.js
@@ -1,59 +1,27 @@
-import React, {createContext, useCallback, useContext, useEffect, useMemo, useReducer, useState} from "react";
+import React, {createContext, useCallback, useContext, useEffect, useMemo, useReducer, useState,useRef} from "react";
 import ReactDOM from "react-dom";
 const rootElement = document.getElementById("root");
 
-//初使用useMemo和React.memo
-//用于防止App组件对子组件进行重复渲染,只要props不变就不会重新渲染子组件
-
+//初始useRef
 function App(){
-    console.log("调用了app")
+    const count = useRef(0)
     const [n,setN] = useState(0)
-    const [m,setM] = useState(0)
-    const addN = ()=>{
-        setN((state)=>{
-            return state+1
-        })
+    const [_,set_] = useState(0)
+    const addN =()=>{
+        count.current+=1
+        set_(Math.random())
     }
-    // const addM = useMemo(()=>{
-    //     return ()=>{
-    //                 setM((state)=>{
-    //                     return state+1
-    //                 })
-    //             }
-    // },[m])
-
-    //useMemo的语法糖
-    const addM =useCallback(
-        ()=>{
-            setM((state)=>{
-                return state+1
-            })
-        },[m]
-    )
-
-
     return (
         <div>
             <div>
-                prend:{n}
+                ref:{count.current}
                 <button onClick={addN}>addN</button>
             </div>
-            {/*<Child2 data={m} onClick={addM}></Child2>*/}
-            <Child2 data={m} onClick={addM}></Child2>
+
         </div>
     )
 
 }
-
-const Child2 = React.memo((props)=>{
-    console.log("调用了CHild")
-    return(
-        <div>
-            Child: {props.data}
-            <button onClick={props.onClick}>addM</button>
-        </div>
-    )
-})
 
 
 
